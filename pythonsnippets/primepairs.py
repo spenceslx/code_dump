ba53b1b1d477642e867666e9257a1ca8244a0591
spenceslx <spenceslx@hotmail.com>
2017-03-04 12:31:51 -0500

    just some structure changes for consistency

---
+++ pythonsnippets/primepairs.py
@@ -0,0 +1,9 @@
+#quick python example#
+#primepairs.py: find the pairs of prime numbers#
+#between a minimum value and a maximum value   #
+
+
+range_min = input("Enter in a minimum value: ")
+range_max = input("Enter in a maximum value: ")
+
+
