615ad47e7598cf6aa895ab3a50eab86e5af57369
spenceslx <spenceslx@hotmail.com>
2017-11-30 13:55:12 -0500

    Not finished, but ran out of time. Spark was seemingly giving issues with line 57 at the .values(), not sure why. Will look into it in the future.

--- EECE5698/AssignmentBonus/GraphMatch.py
+++ EECE5698/AssignmentBonus/GraphMatch.py
@@ -19,14 +19,53 @@ def degree (input_file, output_file, spark_context):
         for elem in data:
             print >> fh, elem
 
-def WL (input_file, output_file, spark_context):
+def WLcolor (input_file, output_file, spark_context):
     """Computes the WL coloring of each node:
        Inputs:
        - file containing an edge between two nodes per lineSearch
        - SparkContext
        Creates a file of (node, WL coloring) pairs"""
-    data = spark_context.textFile(input_file)\
-                        .map(eval)\
+    #create (node, listof[neighbors]) pairs, both directions
+    edges = spark_context.textFile(input_file)\
+                             .map(eval)\
+                             .flatMap(lambda (node1, node2): [(node1, node2), (node2, node1)])
+    #initilize nodes to have a color of 1
+    nodeColors = spark_context.textFile(input_file)\
+                              .map(eval)\
+                              .flatMap(lambda (node1, node2): [(node1, 1), (node2, 1)])\
+                              .reduceByKey(lambda (x,y): 1)
+    #pass to colors (a recursive function) which returns an RDD of (node, color) pairs
+    nodeColors = color(edges, nodeColors, 1).collect()
+    with open(output_file, 'w') as fh:
+        for elem in nodeColors:
+            print >> fh, elem
+
+def color(edges, nodeColors, numOfColors):
+    """Computes the WL coloring of each node:
+       Inputs:
+       - RDD containing (node1, node2) edge pairs
+       - RDD containing (node, color) pairs
+       Creates an RDD of (node, WL coloring) pairs"""
+    #use the Graph-Parallel method as described in lecture 12
+    nodeColors = edges.join(nodeColors)\
+                      .values()\
+                      .combineByKey((lambda x: list(x)),\
+                                    (lambda x, y: x.append(y)),\
+                                    (lambda x, y: x + y))\
+                      .map(lambda (node, colors): (node, hash(sort(colors))))
+                      
+    checkNumOfColors = nodeColors.values()\
+                                 .distinct()\
+                                 .count()
+
+    if (checkNumOfColors != numOfColors):
+        color(edges, nodeColors, checkNumOfColors)
+    else:
+        return nodeColors
+
+
+
+
 
 
 
@@ -41,3 +80,6 @@ if __name__ == "__main__":
 
     #print 'Computing degree of',args.Graph
     #degree(args.Graph, args.outputfile, sc)
+
+    print 'Computing coloring of',args.Graph
+    WLcolor(args.Graph, args.outputfile, sc)
